ea72be7963a90452b4e2c76c5adb99c3a87a5c06
jayud <jayud@DESKTOP-T7IHCJ8>
2023-04-18 12:12:47 +0530

    New Test Cases added

--- LoginScreensCheck/src/test/java/saucedemoTesting/LoginScreenTest.java
+++ LoginScreensCheck/src/test/java/saucedemoTesting/LoginScreenTest.java
@@ -10,63 +10,46 @@ import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class LoginScreenTest 
-{
+public class LoginScreenTest {
 	WebDriver driver;
 	String url = "https://www.saucedemo.com";
 	String afterLogin;
-		
+
 	@BeforeSuite
-	public void launchBrowser()
-	{
-		
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\jayud\\Downloads\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();		
-	}	
-	
+	public void launchBrowser() {
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Users\\jayud\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
 	@AfterSuite
-	public void closeBrowser()
-	{
+	public void closeBrowser() {
 		driver.close();
 	}
-	
+
 	@DataProvider(name = "TestData")
-	public Object[][] testData()
-	{
-		Object[][] testdata = 
-		{
-			{"standard_user","secret_sauce"},
-			{"locked_out_user","secret_sauce"},
-			{"problem_user", "secret_sauce"},
-			{"performance_glitch_user","secret_sauce",}
-		};
-		
+	public Object[][] testData() {
+		Object[][] testdata = { { "standard_user", "secret_sauce" }, { "locked_out_user", "secret_sauce" },
+				{ "problem_user", "secret_sauce" }, { "performance_glitch_user", "secret_sauce", } };
+
 		return testdata;
 	}
-	
-	
+
 	@Test(dataProvider = "TestData")
-	public void loginScreenTest(String user, String pass)
-	{
+	public void loginScreenTest(String user, String pass) {
 		driver.get(url);
 		driver.findElement(By.id("user-name")).sendKeys(user);
 		driver.findElement(By.id("password")).sendKeys(pass);
 		driver.findElement(By.id("login-button")).click();
-	
-		
-		try
-		{
-			WebElement afterLoginE  = driver.findElement(By.xpath("//*[@class='app_logo']"));	
+
+		try {
+			WebElement afterLoginE = driver.findElement(By.xpath("//*[@class='app_logo']"));
 			afterLogin = afterLoginE.getText();
-		}
-		
-		catch(Exception e)
-		{
+		} catch (Exception e) {
 			Assert.assertFalse(true);
 		}
-		
-		
+
 		Assert.assertEquals(afterLogin, "Swag Labs");
 
-	}	
+	}
 }
